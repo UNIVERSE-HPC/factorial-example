dd6e91929fc7d9b1d5872319800e502e9c1f8029
Steve Crouch <s.crouch@software.ac.uk>
2022-12-06 13:03:00 +0000

    Updated unittest test case

--- tests/test_factorial.py
+++ tests/test_factorial.py
@@ -2,10 +2,7 @@ import unittest
 from mymath.factorial import factorial
 
 
-class TestFactorial(unittest.TestCase):
+class TestFactorialFunctions(unittest.TestCase):
 
     def test_3(self):
         self.assertEqual(factorial(3), 6)
-
-    def test_5(self):
-        self.assertEqual(factorial(5), 120)
