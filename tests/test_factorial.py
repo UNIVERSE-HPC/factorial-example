1c4fd1d4b27646210d69e8e5bdb3f0c5aa08f6ae
Steve Crouch <s.crouch@software.ac.uk>
2022-12-06 12:52:49 +0000

    Initial commit

--- tests/test_factorial.py
+++ tests/test_factorial.py
@@ -1,5 +1,11 @@
+import unittest
 from mymath.factorial import factorial
 
 
-def test_factorial_3():
-    assert factorial(3) == 6
+class TestFactorial(unittest.TestCase):
+
+    def test_3(self):
+        self.assertEqual(factorial(3), 6)
+
+    def test_5(self):
+        self.assertEqual(factorial(5), 120)
